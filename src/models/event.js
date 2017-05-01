8c9213b4ea964e47253f4f70c8b7740d3164aca1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-01 21:58:36 +0900

    Create event model

---
+++ src/models/event.js
@@ -0,0 +1,36 @@
+const Model = require('./index');
+
+const Event = Object.assign({
+  data: require('../data/events'),
+  schema: {
+    id: {
+      type: "number",
+    },
+    eventTypeId: {
+      type: "number",
+    },
+    name: {
+      type: "string",
+    },
+    url: {
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+    schedule: {
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+    keywords: {
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+  },
+}, Model);
+
+module.exports = Event;
