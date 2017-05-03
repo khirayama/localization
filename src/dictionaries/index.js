b6b383321d0ffc8150c7cbc3ee5adc4ed0bffbbf
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-03 11:42:25 +0900

    Create dictionaries/index.js

---
+++ src/dictionaries/index.js
@@ -0,0 +1,106 @@
+const dic = {
+  // keyword
+  // - season
+  SPRING: {
+    en: 'spring',
+    ja: '春',
+  },
+  SUMMER: {
+    en: 'summer',
+    ja: '夏',
+  },
+  AUTUMN: {
+    en: 'autumn',
+    ja: '秋',
+  },
+  WINTER: {
+    en: 'winter',
+    ja: '冬',
+  },
+  // - month
+  JANUARY: {
+    en: 'january',
+    ja: '1月',
+  },
+  FEBRUARY: {
+    en: 'february',
+    ja: '2月',
+  },
+  MARCH: {
+    en: 'march',
+    ja: '3月',
+  },
+  APRIL: {
+    en: 'april',
+    ja: '4月',
+  },
+  MAY: {
+    en: 'may',
+    ja: '5月',
+  },
+  JUNE: {
+    en: 'june',
+    ja: '月',
+  },
+  JULY: {
+    en: 'july',
+    ja: '7月',
+  },
+  AUGUST: {
+    en: 'august',
+    ja: '8月',
+  },
+  SEPTEMBER: {
+    en: 'september',
+    ja: '9月',
+  },
+  OCTOBER: {
+    en: 'october',
+    ja: '10月',
+  },
+  NOVEMBER: {
+    en: 'november',
+    ja: '11月',
+  },
+  DECEMBER: {
+    en: 'december',
+    ja: '12月',
+  },
+  // - schedule
+  ANNUAL: {
+    en: 'annual',
+    ja: '毎年',
+  },
+
+  // event type
+  EVENT: {
+    en: 'event',
+    ja: 'イベント',
+  },
+  LIVE: {
+    en: 'live',
+    ja: 'ライブ',
+  },
+
+  // event
+  HAKATA_DONTAKU: {
+    en: 'hakata dontaku',
+    ja: '博多どんたく港まつり',
+  },
+  HAKATA_GION_YAMAKASA: {
+    en: 'hakata gion yamakasa',
+    ja: '博多祇園山笠',
+  },
+
+  // place
+  PORT_OF_HAKATA_CENTRAL_PIER: {
+    en: 'port of hakata central pier',
+    ja: '中央ふ頭',
+  },
+  HAKATA_KUSHIDA_JINJA: {
+    en: 'kushida jinja',
+    ja: '櫛田神社',
+  },
+};
+
+module.exports = dic;
