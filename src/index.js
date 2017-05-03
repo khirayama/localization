e35bb44d883b730e8a2f222af48c0b97365dc683
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-03 11:57:30 +0900

    Create files for client

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const index = require('./index.json');
+const SeachEngine = require('./search-engine/search-engine');
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log(`Start app at ${new Date()}`);
+
+  const searchEngine = new SeachEngine(index);
+
+  const result = searchEngine.search('夏');
+  console.log(result);
+});
