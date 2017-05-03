cf8a63066ff76b4f80d3d0eb13f58635b71d1c34
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-04 06:53:55 +0900

    Create src/server.js

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const path = require('path');
+
+const express = require('express');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+const template = require('./template');
+
+const HomePage = require('./components/home-page');
+
+const app = express();
+const server = http.createServer(app);
+
+const port = 3000;
+const host = process.env.HOST || '127.0.0.1';
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send(
+    template(
+      'Localization',
+      ReactDOMServer.renderToString(<HomePage />),
+      {}
+    )
+  );
+});
+
+server.listen(port, host);
