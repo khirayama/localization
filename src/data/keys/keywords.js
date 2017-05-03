126199c3a9193cd6fe733a538147ad00d3aaff69
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-03 11:41:06 +0900

    Move data/constants -> data/keys

---
+++ src/data/keys/keywords.js
@@ -0,0 +1,24 @@
+const keywords = {
+  season: {
+    SPRING: 'SPRING',
+    SUMMER: 'SUMMER',
+    AUTUMN: 'AUTUMN',
+    WINTER: 'WINTER',
+  },
+  month: {
+    JANUARY: 'JANUARY',
+    FEBRUARY: 'FEBRUARY',
+    MARCH: 'MARCH',
+    APRIL: 'APRIL',
+    MAY: 'MAY',
+    JUNE: 'JUNE',
+    JULY: 'JULY',
+    AUGUST: 'AUGUST',
+    SEPTEMBER: 'SPPTEMBER',
+    OCTOBER: 'OCTOBER',
+    NOVEMBER: 'NOVEMBER',
+    DECEMBER: 'DECEMBER',
+  },
+};
+
+module.exports = keywords;
