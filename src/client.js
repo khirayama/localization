1bf2f47d0602eef9ba9b6467e791c05e2c2feceb
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-04 06:53:34 +0900

    Use react component in client

--- src/client.js
+++ src/client.js
@@ -1,11 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
 const index = require('./index.json');
 const SeachEngine = require('./search-engine');
+
+const HomePage = require('./components/home-page');
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log(`Start app at ${new Date()}`);
 
   const searchEngine = new SeachEngine(index);
 
-  const result = searchEngine.search('夏');
-  console.log(result);
+  ReactDOM.render(<HomePage />, document.querySelector('.application'));
 });
