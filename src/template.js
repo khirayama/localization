e7343971df551f51aeb7eb89480d755fa6edba39
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-05-05 22:15:55 +0900

    Split style code

--- src/template.js
+++ src/template.js
@@ -1,3 +1,111 @@
+const style = `
+  /* reset */
+  html,
+  body,
+  section,
+  header,
+  footer,
+  div,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a,
+  span,
+  ul,
+  li,
+  label,
+  input,
+  textarea,
+  select,
+  button,
+  table,
+  thead,
+  tbody,
+  tr,
+  th,
+  td,
+  i {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    border-radius: 0;
+    font-weight: inherit;
+    font-style: normal;
+    font-size: inherit;
+    line-height: 1.4;
+    vertical-align: baseline;
+    font-feature-settings: "palt";
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+  }
+  ul {
+    list-style: none;
+  }
+  h1 {
+    font-size: 2rem;
+  }
+  h2 {
+    font-size: 1.8rem;
+  }
+  h3 {
+    font-size: 1.6rem;
+  }
+  h4 {
+    font-size: 1.4rem;
+  }
+  h5 {
+    font-size: 1.2rem;
+  }
+  h6 {
+    font-size: 1rem;
+  }
+  small {
+    font-size: 0.8rem;
+  }
+  input,
+  textarea,
+  select,
+  button {
+    font-size: inherit;
+    font-family: inherit;
+  }
+  form,
+  input,
+  textarea,
+  select,
+  button,
+  a {
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-user-select: auto;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  /* application */
+  html,
+  body,
+  .application {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+  html {
+    color: #666;
+    font-family: sans-serif;
+  }
+`;
+
 function template(title, content, state) {
   return (`
 <!DOCTYPE html>
@@ -16,113 +124,7 @@ function template(title, content, state) {
     <meta name="apple-mobile-web-app-capable" content="yes">
     <link rel="apple-touch-icon" sizes="76x76" href="/images/icon-ios.png">
     <script src="/bundle.js" defer></script>
-    <style>
-    /* reset */
-    html,
-    body,
-    section,
-    header,
-    footer,
-    div,
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6,
-    p,
-    a,
-    span,
-    ul,
-    li,
-    label,
-    input,
-    textarea,
-    select,
-    button,
-    table,
-    thead,
-    tbody,
-    tr,
-    th,
-    td,
-    i {
-      box-sizing: border-box;
-      margin: 0;
-      padding: 0;
-      outline: 0;
-      border: 0;
-      border-radius: 0;
-      font-weight: inherit;
-      font-style: normal;
-      font-size: inherit;
-      line-height: 1.4;
-      vertical-align: baseline;
-      font-feature-settings: "palt";
-      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-      -webkit-touch-callout: none;
-      -webkit-user-select: none;
-    }
-    ul {
-      list-style: none;
-    }
-    h1 {
-      font-size: 2rem;
-    }
-    h2 {
-      font-size: 1.8rem;
-    }
-    h3 {
-      font-size: 1.6rem;
-    }
-    h4 {
-      font-size: 1.4rem;
-    }
-    h5 {
-      font-size: 1.2rem;
-    }
-    h6 {
-      font-size: 1rem;
-    }
-    small {
-      font-size: 0.8rem;
-    }
-    input,
-    textarea,
-    select,
-    button {
-      font-size: inherit;
-      font-family: inherit;
-    }
-    form,
-    input,
-    textarea,
-    select,
-    button,
-    a {
-      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-      -webkit-user-select: auto;
-    }
-    a {
-      color: inherit;
-      text-decoration: none;
-      cursor: pointer;
-    }
-
-    /* application */
-    html,
-    body,
-    .application {
-      width: 100%;
-      height: 100%;
-      padding: 0;
-      margin: 0;
-    }
-    html {
-      color: #666;
-      font-family: sans-serif;
-    }
-    </style>
+    <style>${style}</style>
   </head>
   <body>
     <section class="application">${content}</section>
